98b4b29108d6dcf957672763f9f9847374e8568b
Alex Alfonso <alfonsoalexandr@gmail.com>
2021-12-18 19:02:24 +0300

    item is done

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :user
+  def self.find_by_category(category)
+    cat = Category.find_by(title: category)
+    Test.where(category_id: cat.id).order(title: :desc).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def show_tests_by_level(level)
+    tests = Result.where(user_id: id).pluck(:test_id)
+    Test.where(id: tests).and(Test.where(level: level))
+  end
 end
