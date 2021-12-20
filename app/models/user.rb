aa26db2c9dbee0f861d7922cd59481fa45acc515
Alex Alfonso <alfonsoalexandr@gmail.com>
2021-12-20 14:50:20 +0300

    item is done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   def show_tests_by_level(level)
-    Test.joins('JOIN results ON tests.id = results.test_id').where('results.user_id = :user_id',
-                                                                   user_id: id).and(Test.where(level: level))
+    Test.joins('JOIN results ON tests.id = results.test_id')
+    .where(results: { user_id: id})
+    .where(level: level)
   end
 end
