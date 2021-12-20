57b75627c63322978bc9d5d86c2e3dccf7b2efb7
Alex Alfonso <alfonsoalexandr@gmail.com>
2021-12-20 12:46:16 +0300

    some fixes in class test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,8 @@
 class Test < ApplicationRecord
-  def self.find_by_category(category)
-    Test.joins('JOIN categories ON tests.category_id = categories.id').where('categories.title = :category',
-                                                                             category: category).order(title: :desc).pluck(:title)
+  def self.tests_by_category_name(category_name)
+    Test.joins('JOIN categories ON tests.category_id = categories.id')
+    .where(categories: { title: category_name })
+    .order(title: :desc)
+    .pluck(:title)
   end
 end
