9952ba482d53cb38a65d0f539b74838a888860e0
Alex Alfonso <alfonsoalexandr@gmail.com>
2021-12-29 01:49:27 +0300

    fix valid

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,13 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :validate_answers_count
+  validate :validate_answers_count, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
 
   def validate_answers_count
-    errors.add(:question) if question.answers.count > 4
+    errors.add(:answer) if question.answers.count > 3
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,11 +4,4 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :body, presence: true
-  validate :validate_answers_count
-
-  private
-
-  def validate_answers_count
-    errors.add(:question) if answers.count > 4
-  end
 end
