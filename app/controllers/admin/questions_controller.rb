6699a7aa41a16ad84518bac3adc46ee6b07bf76f
Alex Alfonso <alfonsoalexandr@gmail.com>
2022-01-18 17:02:04 +0300

    change base_controller to question and answer model

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,4 +1,4 @@
-class Admin::QuestionsController < ApplicationController
+class Admin::QuestionsController < Admin::BaseController
   before_action :find_test, only: %i[create new]
   before_action :find_question, only: %i[show destroy edit update]
 
